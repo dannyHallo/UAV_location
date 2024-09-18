aa291eceaaaf4f55ec5c337f457d6a626d2f0f88
ruitian <yangruitian.yrt@bytedance.com>
2024-09-19 00:52:51 +0800

    sync

--- src/Config.py
+++ src/Config.py
@@ -6,15 +6,16 @@ receiver_position_2 = np.array([100, 100])
 receiver_position_3 = np.array([300, 150])
 
 train_num_of_lines_to_generate = 102400
-test_set_num = 512
-train_step_count_per_line = 5
+test_set_long_num = 512
+test_set_short_num = 2048
+step_count_per_line = 5
 length_per_step = 0.2
 angle_change_limit_per_step = 0.1
 a_b_distance = 0.05
 
 epoch = 1000
 train_batch_size = 128
-test_batch_size = test_set_num
+# test_batch_size = test_set_num
 
 learning_rate = 0.0003
 
