90fc4dc40076cf74494233df959e1f7f257fe0da
hyrissq <157707297+CeciliaFighting@users.noreply.github.com>
2025-04-14 15:07:03 +0800

    temp-temp

--- src/Config.py
+++ src/Config.py
@@ -5,9 +5,9 @@ receiver_position_1 = np.array([200, 0])
 receiver_position_2 = np.array([100, 100])
 receiver_position_3 = np.array([300, 150])
 
-t1=0.5
-t2=0.2
-train_num_of_lines_to_generate = 5
+t1=0.01
+t2=0.01
+train_num_of_lines_to_generate = 10000
 test_num_of_lines_to_generate = 1
 step_count_per_line = 5
 length_per_step = 0.2
