448900741882e89e83ea91128c64dd5de4a4f100
danny <ruitian0716@gmail.com>
2025-05-21 02:00:31 +0800

    change of config to real thing

--- src/config.py
+++ src/config.py
@@ -1,19 +1,18 @@
 stage_1_train_set_path = "cache/stage_1_train.pt"
-stage_1_test_set_path ="cache/stage_1_test.pt"
+stage_1_test_set_path = "cache/stage_1_test.pt"
 
-time_interval=0.01
-train_num_of_lines_to_generate_per_region = 1
-test_num_of_lines_to_generate_per_region = 1
-train_detecting_region_nums = 1
-test_detecting_region_nums = 1
-visualize_detecting_region_nums = 2
+time_interval = 0.01
+train_num_of_lines_to_generate_per_region = 25
+test_num_of_lines_to_generate_per_region = 2
+train_detecting_region_nums = 800
+test_detecting_region_nums = 20
+visualize_detecting_region_nums = 5
 train_seed = 42
 test_seed = 66
 visualize_seed = 77
+
 # step_count_per_line = 5
 # length_per_step = 0.2
-
-
 
 epoch = 50
 train_batch_size = 128
@@ -26,4 +25,4 @@ fc = 6e9
 c = 3e8
 v = 30
 
-optimizer = "Adam" # ["LBFGS" / "Adam"]
+optimizer = "Adam"  # ["LBFGS" / "Adam"]
