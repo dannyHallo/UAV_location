46770f115e9c6ed0e152f08686c093cc86fb109c
danny <ruitian0716@gmail.com>
2025-06-18 12:17:50 +0800

    sync

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ time_interval = 0.01
 
 num_detecting_regions = 1
 visualize_num_detecting_regions = 1
-num_lines_to_generate_per_region = 20000
+num_lines_to_generate_per_region = 100000
 
 line_seed = 42
 visualize_line_seed = 0
